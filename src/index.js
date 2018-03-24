6dc8d9ca5c2047546d4b2d72d7ea40917f32532e
Ben Pohl <pohl989@gmail.com>
2018-03-23 18:29:49 -0600

    loaded Youtube API

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 // import react into the file
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import TYSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDQqwBUPzS4Cw5o_Qz9TeSaCjw5RRS23I4';
 
-// Create a new component. this Component should produce some HTML
+TYSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+})
 
+// Create a new component. this Component should produce some HTML
 const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this component's generated HTML and put it on the page (In the DOM)
