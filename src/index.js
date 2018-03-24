5d637c9bd64b26d12dd7b79f6bd5744c201b94c2
Ben Pohl <pohl989@gmail.com>
2018-03-24 15:09:35 -0600

    morning update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 // import react into the file
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 import TYSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDQqwBUPzS4Cw5o_Qz9TeSaCjw5RRS23I4';
 
-TYSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-})
-
 // Create a new component. this Component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    TYSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // this.setState({ videos: videos }) ES6 can do this
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's generated HTML and put it on the page (In the DOM)
